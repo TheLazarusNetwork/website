57884ce00c5aab4a7c03812e29dc203ff6d5d4e0
emily876 <singhdevsi943@gmail.com>
2022-02-07 23:43:47 +0530

    services page content changed

--- pages/services.js
+++ pages/services.js
@@ -111,12 +111,12 @@ const services = () => {
 
             <div className=" pt-10">
               <div className="mt-10 text-center">
-                <h1 className="text-4xl font-bold text-gray-800">Lazarus Network Services</h1>
-                <p className="text-lg mt-3 font-semibold">Enterprise Grade Cybersecurity Solutions for Individuals and Businesses. At Lazarus we leverage AI And Blockchain technologies For cyber defense to protect you and your enterprise from online viruses while you Work From Home.</p>
+                <h1 className="text-4xl font-bold text-gray-800">Cybersecurity Solutions</h1>
+                {/* <p className="text-lg mt-3 font-semibold">Enterprise Grade Cybersecurity Solutions for Individuals and Businesses. At Lazarus we leverage AI And Blockchain technologies For cyber defense to protect you and your enterprise from online viruses while you Work From Home.</p> */}
               </div>
-              <div className="mt-10 text-center">
+              {/* <div className="mt-10 text-center">
                 <h4 className="text-2xl font-bold text-gray-800">Virtual Private Networks</h4>
-              </div>
+              </div> */}
             </div>
 
             <div className="flex flex-wrap items-center mt-32">
@@ -141,12 +141,12 @@ const services = () => {
                     <p className="px-4 text-center text-sm ">Anonymous VPN uses P2P VPN Solution for bypassing IP Fingerprinting. Get 100% anonymous internet browsing around the world including restricted regions.
                       <br /><br />Lazarus VPN gives you encrypted internet surfing and prevents hackers and malware from accessing your data between your device and the internet.</p>
                     <ul className="text-center py-3">
-                      <li><a href="#" className="font-semibold">Secure online browsing</a></li>
-                      <li><a href="#" className="font-semibold">Access to Restricted Content</a></li>
-                      <li><a href="#" className="font-semibold">Network-wide protection</a></li>
-                      <li><a href="#" className="font-semibold">Block in-app advertisements</a></li>
-                      <li><a href="#" className="font-semibold">Improve network performance</a></li>
-                      <li><a href="#" className="font-semibold">Monitor statistics</a></li>
+                      <li><a className="font-semibold">Secure online browsing</a></li>
+                      <li><a className="font-semibold">Access to Restricted Content</a></li>
+                      <li><a className="font-semibold">Network-wide protection</a></li>
+                      <li><a className="font-semibold">Block in-app advertisements</a></li>
+                      <li><a className="font-semibold">Improve network performance</a></li>
+                      <li><a className="font-semibold">Monitor statistics</a></li>
                     </ul>
                     <div className="text-center bg-pink-600 ">
                     <Link href="/pricing">
@@ -194,11 +194,11 @@ const services = () => {
                   <p className="px-4 text-center text-sm ">Lazarus VPN is an encrypted tunneling solution for end users offering WireGuard® Tunneling, secured by Ed25519 Private Key Encryption, Multi Region Network, Peer to Peer network access along with network layer customizable firewall. Get higher bandwidth and optimized performance for enterprise software usages requiring good latency.
                     <br /><br />Lazarus Firewall is incorporated on the network layer (DNS Layer) for detecting and eliminating cyber threats which try to attack the application system.</p>
                   <ul className="text-center py-4">
-                    <li><a href="#" className="font-semibold">All the features of ANONYMOUS VPN</a></li>
-                    <li><a href="#" className="font-semibold">
+                    <li><a className="font-semibold">All the features of ANONYMOUS VPN</a></li>
+                    <li><a className="font-semibold">
                       Dedicated Network - Secure Tunnel</a></li>
-                    <li><a href="#" className="font-semibold">Network Layer Customizable Firewall</a></li>
-                    <li><a href="#" className="font-semibold">Dedicated Bandwidth and Performance - Not Shared</a></li>
+                    <li><a className="font-semibold">Network Layer Customizable Firewall</a></li>
+                    <li><a className="font-semibold">Dedicated Bandwidth and Performance - Not Shared</a></li>
                   </ul>
                   <div className="text-center bg-pink-600 ">
                   <Link href="/pricing">
@@ -218,7 +218,7 @@ const services = () => {
                       <div className="flex items-center">
                         <div>
                           <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
-                            <i className="fas fa-fingerprint"></i>
+                            <i className="fa fa-check"></i>
                           </span>
                         </div>
                         <div>
@@ -232,7 +232,7 @@ const services = () => {
                       <div className="flex items-center">
                         <div>
                           <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
-                            <i className="fab fa-html5"></i>
+                            <i className="fa fa-check"></i>
                           </span>
                         </div>
                         <div>
@@ -246,7 +246,7 @@ const services = () => {
                       <div className="flex items-center">
                         <div>
                           <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
-                            <i className="far fa-paper-plane"></i>
+                            <i className="fa fa-check"></i>
                           </span>
                         </div>
                         <div>
@@ -264,8 +264,8 @@ const services = () => {
         </section>
         <hr className="mt-10" />
 
-        <section className="pt-20 pb-20">
-          <div className="container mx-auto px-4">
+        {/* <section className="pt-20 pb-20">
+          <div className="container mx-auto px-4"> */}
             {/* <div className="flex flex-wrap justify-center text-center mb-24">
               <div className="w-full lg:w-6/12 px-4">
                 <h2 className="text-4xl font-semibold">Here are our heroes</h2>
@@ -276,7 +276,7 @@ const services = () => {
                 </p>
               </div>
             </div> */}
-            <div className="flex flex-wrap pb-20">
+            {/* <div className="flex flex-wrap pb-20">
               <div className="w-full md:w-6/12 lg:w-6/12 lg:mb-0 mb-12 px-4">
                 <div className="px-6">
                   <div className="pt-6 text-center">
@@ -379,11 +379,11 @@ const services = () => {
                     Explore More <i className="ti-arrow-right"></i>
                   </a>
                   </div>
-                </div>
-              </div>
+                </div> */}
+              {/* </div>
             </div>
           </div>
-        </section>
+        </section> */}
 
         {/* <section className="relative block bg-blueGray-800">
 
@@ -503,187 +503,6 @@ const services = () => {
           </div>
         </div>
       </div> */}
-
-      {/* <div className="spacer feature4">
-        <Container> */}
-          {/* <Row className="justify-content-center bg-gray-100">
-            <Col md="7" className="text-center pt-10 pb-10">
-              <h2 className="title">Do you want to be sure your IT infrastructure is completely secure?</h2>
-              <h6 className="subtitle">
-                Get detailed insight into the actual cybersecurity of your IT infrastructure and essential apps with Lazarus’s security testing as a service, along with a set of advice on how to make them as hacker-proof as possible.
-              </h6>
-              <h2 className="title">Cybersecurity Consulting</h2>
-              <h6 className="subtitle">
-                Cybersecurity consultancy assists businesses in proactively identifying security risks and holes in their IT infrastructures. We will be providing full-service information security consulting services to clients.
-              </h6>
-              <h2 className="title">Improve your information security by leveraging our capabilities to:</h2>
-              <h6 className="subtitle">
-                Examine how well your cyber-environment is secured and strong against APTs, ransomware, and other threats.
-              </h6>
-              <h6 className="subtitle">
-                To avoid possible attacks, identify current weaknesses in your network.
-              </h6>
-              <h6 className="subtitle">
-                Improve the performance of your data security solutions.
-              </h6>
-              <h6 className="subtitle">
-                Detects cyberattacks on your network in real time.
-              </h6>
-              <h6 className="subtitle">
-                Maintain the security of your important data.
-              </h6>
-              <br />
-              <h6 className="subtitle">
-                Dont jeopardize your security or productivity. Allow access when it is required and deny it when it is not.
-              </h6>
-            </Col>
-          </Row> */}
-          {/* <Row className="m-t-40 pl-30 pt-30 pr-30">
-            <Col md="6" className="wrap-feature4-box">
-              <Card>
-                <CardBody>
-                  <div className="icon-round bg-light-info">
-                    <i className="fa fa-star"></i>
-                  </div>
-                  <h5 className="font-medium">Storage</h5>
-                  <h5 className="font-medium">Lazarus Drive</h5>
-                  <p className="m-t-20 mb-4">
-                    Lazarus Drive is a private file storage service on the cloud and the connected devices based on the IPFS - A peer-to-peer hypermedia protocol to publish files on the private Internet without worrying if they will be tampered with, and share them with others within the network.
-                  </p>
-                  <a href="#" className="linking text-themecolor">
-                    Explore More<i className="ti-arrow-right"></i>
-                  </a>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col md="6" className="wrap-feature4-box">
-              <Card>
-                <CardBody>
-                  <div className="icon-round bg-light-info">
-                    <i className="fa fa-check-circle"></i>
-                  </div>
-                  <h5 className="font-medium">Nodes</h5>
-                  <h5 className="font-medium">GATEWAY</h5>
-                  <p className="m-t-20 mb-4">
-                    Smart Hardware Device offering secure WiFi and all the above mentioned services. Best suited for Smart Home and Office Users who wish to have the network and data hosted locally.
-                  </p>
-                  <a className="linking text-themecolor" href="#">
-                    Explore More <i className="ti-arrow-right"></i>
-                  </a>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row> */}
-          {/* <Row className="m-t-0 pl-30 pr-30 pt-30">
-            <Col md="6" className="wrap-feature4-box">
-              <Card>
-                <CardBody>
-                  <div className="icon-round bg-light-info">
-                    <i className="fa fa-star"></i>
-                  </div>
-                  <h5 className="font-medium">Communication</h5>
-                  <p className="m-t-10 mb-4">
-                    Changing the way you communicate. Stake control of your data privacy with end-to-end encryption.
-                  </p>
-
-                  <div className="round-social light">
-                    <a className="link">
-                      <i className="fa fa-facebook"></i>
-                    </a>
-                    <a className="link">
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                    <a className="link">
-                      <i className="fa fa-google-plus"></i>
-                    </a>
-                  </div>
-
-                  <h3 className="my-2">Lazarus Stream</h3>
-                  <h5>Change the way you Stream</h5>
-                  <p className="mt-2 mb-4">
-                    Lazarus Stream leverages IPFS Network for the content delivery and relay of streams for hosting the video calls within the private network created with Lazarus VPN. It provides a secure and private video streaming service using the private decentralized IPFS network. Your communication is protected from outside attacks, so with our end-to-end encryption your conversations will always be private.
-                  </p>
-
-                  <a href="#" className="linking text-themecolor">
-                    Explore More<i className="ti-arrow-right"></i>
-                  </a>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col md="6" className="wrap-feature4-box">
-              <Card>
-                <CardBody>
-                  <div className="icon-round bg-light-info">
-                    <i className="fa fa-check-circle"></i>
-                  </div>
-                  <h5 className="font-medium">Application Security</h5>
-
-                  <div className="round-social light my-2">
-                    <a className="link">
-                      <i className="fa fa-facebook"></i>
-                    </a>
-                    <a className="link">
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </div>
-
-                  <h3>Lazarus NetGuard</h3>
-                  <p className="mt-2 mb-2">
-                    NetGuard enables customers to preemptively protect their online activities from advanced threats in a collaborative way, with a decentralized and tamper-proof process using blockchain technology. Most current solutions rely on complex AI technology, which may sometimes return false positives and using our decentralized solution this process can be reinforced and weed out the false positives
-                  </p>
-                  <h3>Always on Guard</h3>
-                  <p className="mt-2 mb-4">
-                    Physical and network boundaries have vanished, but this should not be an impediment to your business.
-                  </p>
-                  <a className="linking text-themecolor" href="#">
-                    Explore More <i className="ti-arrow-right"></i>
-                  </a>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row> */}
-        {/* </Container>
-      </div> */}
-      {/* <div className="" style={{marginTop:-80}}>
-        <Container className="feature30">
-          <Row>
-            <Col className="text-center">
-              <Card className="card-shadow bg-gray-100">
-                <CardBody>
-                  <div className="p-20">
-                    <h3 className="title">
-                      Blockchain
-                    </h3>
-                    <img
-                      src="https://cdn.pixabay.com/photo/2017/12/31/11/57/block-chain-3052119_960_720.png"
-                      className="rounded img-responsive"
-                      alt="wrappixel"
-                      style={{ width: 200 }}
-                    />
-                    <h3 className="subtitle">
-                      ETHEREUM
-                    </h3>
-                    <p>
-                      Blockchain Network is based on Ethereum private blockchain network using PoA as a Consensus Algorithm. The Smart Contracts used are for Token creation, domain/network registration, VPN management and Firewall rule management. Based on the enterprise needs a separate blockchain network can be deployed on their dedicated network to integrate the business requirements.
-                      <br /><br />
-                      The public network currently in use is Görli Testnet - The first proof-of-authority cross-client testnet, synching Parity Ethereum, Geth, Nethermind, Hyperledger Besu (formerly Pantheon), and EthereumJS. This testnet is a community-based project, completely open-source, naturally.
-                    </p>
-                    <a
-                      className="btn btn-danger btn-md btn-arrow m-t-20"
-                      href="#"
-                    >
-                      <span>
-                        Explore More <i className="ti-arrow-right"></i>
-                      </span>
-                    </a>
-                  </div>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </div> */}
-
 
     </div>
   )
